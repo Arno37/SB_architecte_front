a7e1c5fb685d8e8850e848e59421ab4065ac714e
arnaudrambourg <arnaudrambourg@gmail.com>
2024-02-19 18:16:17 +0100

    implements loggin page

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
 async function getWorks() {
     try {
         const response = await fetch('http://localhost:5678/api/works/');
+        
         const dataWorks = await response.json();
-
+        
         const gallery = document.querySelector('.gallery');
-
+        console.log(gallery)
         for (const currentWork of dataWorks) {
             const figure = document.createElement('figure');
            
@@ -29,14 +30,35 @@ async function getWorks() {
 async function getCategories() {
     try {
         const response = await fetch('http://localhost:5678/api/categories/');
-        const data = await response.json();// Affiche la réponse dans la console
+        
+        const dataCategories = await response.json();// Affiche la réponse dans la console
+        
+        const filters = document.querySelector('.filters');
+       
+        for (const currentCategories of dataCategories) {
+            
+            const button = document.createElement('button');
+            button.textContent = currentCategories.name;
+
+            filters.appendChild(button)
+
+        }
+
+
+
     } catch (error) {
         console.error('Une erreur s\'est produite :', error);
     }
+   
+    
 }
+
+
 // Appel de la fonction fetchData dès que le document est chargé
 document.addEventListener('DOMContentLoaded', () => {
     getWorks();
     getCategories();
+
+
 })
 
