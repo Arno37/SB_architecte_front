48a855b83ddb16be4a97abfc6aca095c10ed8c16
arnaudrambourg <arnaudrambourg@gmail.com>
2024-02-21 19:56:24 +0100

    JS set up for  categories

--- javascript/script.js
+++ javascript/script.js
@@ -30,6 +30,25 @@ async function getWorks() {
 }
 
 function setfilter(categoryId){
+    const gallery = document.querySelector('.gallery');
+    const figures= gallery.querySelectorAll('figure')
+    console.log('boutonclique ' , categoryId)
+
+    for  (const figure of figures) {
+        console.log(figure.dataset.category)
+
+        if(figure.dataset.category == categoryId){
+            figure.style.display='block'
+        }
+        else if (categoryId == 0){
+            figure.style.display='block'
+        }
+        else {
+            figure.style.display='none'
+        }
+    }
+    
+
 
 
 }
@@ -37,11 +56,18 @@ function setfilter(categoryId){
 
 async function getCategories() {
     try {
+        const filters = document.querySelector('.filters');
+        const button = document.createElement('button');
+        button.textContent = 'Tous';
+        button.addEventListener("click",()=>setfilter(0))
+        filters.appendChild(button)
+
+
         const response = await fetch('http://localhost:5678/api/categories/');
         
         const dataCategories = await response.json();// Affiche la réponse dans la console
         
-        const filters = document.querySelector('.filters');
+        
 
         
         for (const currentCategory of dataCategories) {
@@ -56,16 +82,66 @@ async function getCategories() {
 }
 
 
-
-
-
-
     } catch (error) {
         console.error('Une erreur s\'est produite :', error);
     }
    
+    async function loginUser(email, password) {
+        try {
+            
+            const response = await fetch('http://localhost:5678/api-docs/#/default/post_users_login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                    
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+                
+            });
+    
+          
+            if (response.ok) {
+           
+                const data = await response.json();
+                window.location.href = '/accueil.html';
+                
+                return data;
+            } else if (response.status === 401) {
+                
+                throw new Error('Email ou mot de passe incorrect.');
+            } else if (response.status === 404) {
+                
+                throw new Error('Utilisateur non trouvé.');
+            } else {
+               
+                throw new Error('Erreur inattendue.');
+            }
+        } catch (error) {
+            
+            console.error('Une erreur s\'est produite lors de la connexion :', error);
+           
+            return null;
+        }
+    }
+    
+    
+    /*loginUser('exemple@email.com', 'motdepasse')
+        .then(data => {
+            
+            if (data) {
+                console.log('Utilisateur connecté avec succès:', data);
+                
+            } else {
+                console.log('Échec de la connexion.');
+               
+            }
+        });*/
     
 }
+
 
 
 // Appel de la fonction fetchData dès que le document est chargé
