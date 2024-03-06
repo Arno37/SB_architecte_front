1b4ba41e389bd393447e817e5c0dc5260a2d6002
arnaudrambourg <arnaudrambourg@gmail.com>
2024-03-06 20:05:23 +0100

    add delete picture on modal

--- javascript/get_projects.js
+++ javascript/get_projects.js
@@ -1,3 +1,20 @@
+async function deleteItemFromApi (idPhoto) {
+    const userToken = localStorage.getItem('token');
+    
+    const response = await fetch(`http://localhost:5678/api/works/${idPhoto}`,{
+           method: 'DELETE',
+           headers: {
+               'Authorization': `Bearer ${userToken}`,
+               'Content-Type': 'application/json'
+           }
+       });
+       
+       if (response.ok) {
+         console.log('image supprimee')
+       } else {
+         console.log('un probleme est survenu lors de la suppression')
+       }
+}
 async function fetchPhotosFromAPI() {
     try {
         const response = await fetch('http://localhost:5678/api/works/');
@@ -18,7 +35,10 @@ async function fetchPhotosFromAPI() {
             trashButton.className = 'trash-button';
             trashButton.id = work.id;
             
-            trashButton.addEventListener('click', function() {   
+            trashButton.addEventListener('click', async function() {  
+                
+                await deleteItemFromApi(this.id)
+                
                 divPhoto.remove(); 
             });
 
